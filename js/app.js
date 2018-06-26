85fd4ab2cac60a0cfc0f23f4f66a390770d76347
Susanta Chakraborty <svchost96@gmail.com>
2018-06-26 20:36:54 +0530

    just add the budget value from user input

--- js/app.js
+++ js/app.js
@@ -1,15 +1,34 @@
 //Classes
+class Budget {
+  constructor(budget) {
+    this.budget = Number(budget);
+    this.budgetLeft = this.budget;
+  }
+}
 
+//Related to HTML
+class HTML {
 
+  //Insert the budget when the users submits the prompt
+  insertBudget(amount) {
 
+    //Insert into HTML
+    budgetTotal.innerHTML = `${amount}`;
+    budgetLeft.innerHTML = `${amount}`;
+  }
+
+}
 
 
 
 //Variables
-const addExpenseForm = document.querySelector('#add-expense');
+const addExpenseForm = document.querySelector('#add-expense'),
+  budgetTotal = document.querySelector('span#total'),
+  budgetLeft = document.querySelector('span#left');
 
 let userBudegt, budget;
-
+//Instanciate the HTML Class
+const html = new HTML();
 
 
 
@@ -25,12 +44,17 @@ function eventListeners() {
       window.location.reload();
     }
     else {
+      //After Budget is valid then instanciate the budget class
+      budget = new Budget(userBudegt);
+
+      // Instenciate HTML Class
+      html.insertBudget(budget.budget);
 
     }
   });
 
   //when a new expense added
-  addExpenseForm.eventListeners('submit', function (e) {
+  addExpenseForm.addEventListener('submit', function (e) {
     e.preventDefault();
   });
 }
